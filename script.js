abf6f0b2a77257be8533559b673541fb1f2eb429
unknown <beidi.beer@gmail.com>
2023-09-04 21:14:50 +0100

    fix few bugs

--- script.js
+++ script.js
@@ -1,32 +1,63 @@
+// Define audioElement here at the top
+const audioElement = document.getElementById("sound");
+
 // Load the JSON data (assuming 'fongbe.json' is in the same directory)
-fetch("fongbe.json")
+fetch("fongbeA.json")
   .then((response) => response.json())
   .then((data) => {
     const result = document.getElementById("result");
-    const sound = document.getElementById("sound");
+    const sound = document.getElementById("play");
     const btn = document.getElementById("search-btn");
-    const input = document.getElementById("inp-word")
+    const input = document.getElementById("inp-word");
+
+    const playSound = () => {
+      console.log('clicked');
+      audioElement.load(); // Reload the audio element
+      audioElement.play(); // Play the audio
+    };
 
     // Function to handle the search
     const handleSearch = () => {
       const word = input.value.trim().toLowerCase();
 
       // Find the word entry in the data array
-      const wordEntry = data.find((entry) => entry.word === word);
+      const wordEntry = data[word];
+      console.log(wordEntry);
 
       if (wordEntry) {
         // If the word is found, display its definitions
-        const definitions = wordEntry.definition[0];
+        const definitions = wordEntry.definitions[0].definition;
         document.querySelector('h3').textContent = definitions;
+
+        const audioSource = wordEntry.definitions[0].audio;
+        if (audioSource) {
+          audioElement.src = 'assets/'+ audioSource;
+          document.querySelector('#play').style.display = 'inline'
+
+        } else{
+          document.querySelector('#play').style.display = 'none'
+        }
       } else {
         // If the word is not found, display a message
         document.querySelector('h3').textContent = "Word not found in the dictionary.";
       }
-      document.getElementById("result").style.display = "flex"
+
+      if (wordEntry.definitions[0].examples[0]) {
+        document.querySelector('.fongbe-sentence').textContent = wordEntry.definitions[0].examples[0];
+        document.querySelector('.french-sentence').textContent = wordEntry.definitions[0].examplesFR[0];
+        document.querySelector('.word-example').style.display = 'block'
+      }
+      else {
+        document.querySelector('.word-example').style.display = 'none'
+      }
+
+      document.getElementById("result").style.display = "block";
     };
 
     // Event listener for the "Search" button click
     btn.addEventListener("click", handleSearch);
+
+    sound.addEventListener("click", playSound);
 
     // Event listener for the "Enter" key press in the input field
     input.addEventListener("keypress", (event) => {
@@ -35,3 +66,5 @@ fetch("fongbe.json")
       }
     });
   });
+
+document.addEventListener(''); // You might want to specify an event here
